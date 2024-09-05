066c555fa704aeb1c166cb6dba82c9858879e450
Svetlana Evdokimova <7757331@gmail.com>
2024-09-05 17:27:10 +0300

    fix: fix markup and styles to work without js (Thu, Sep 05, 2024 05:27:10 PM)

--- cssMemeSlider/js/script.js
+++ cssMemeSlider/js/script.js
@@ -4,9 +4,11 @@ window.addEventListener('DOMContentLoaded', () => {
     const img = document.querySelectorAll('.slider_img');
     const dots = document.querySelectorAll('.slider_dot');
     const text = document.querySelectorAll('.slider_text');
+    const texts = document.querySelector('.slider_texts');
 
     function slideToNext(n) {
         imgs.style.transform = `translateX(-${n * img[0].offsetWidth}px)`;
+        texts.style.transform = `translateX(-${n * text[0].offsetWidth}px)`;
         dots[n].classList.add('slider_dot_active');
     }
 
@@ -14,10 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.forEach(d => {
             d.classList.remove('slider_dot_active');
         });
-        dots[0].classList.add('slider_dot_active');
     }
-
-    reset();
 
     dots.forEach((dot, i) => {
         dot.addEventListener('click', (e) => {
